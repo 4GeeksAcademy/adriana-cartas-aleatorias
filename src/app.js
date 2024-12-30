1a56271605efc774c60afc16b836203043119316
adrianallanga <rojas-escobar@hotmail.com>
2024-12-30 18:10:00 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+  let suits = ["♥", "♦", "♠", "♣"];
+
+  let randomValue = values[Math.floor(Math.random() * values.length)];
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  let carta = document.getElementById("carta");
+  let simbolo1 = document.getElementById("simbolo1");
+  let simbolo2 = document.getElementById("simbolo2");
+  let numero = document.getElementById("numero");
+
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    carta.className = "carta heart";
+  } else {
+    carta.className = "carta";
+  }
+
+  numero.innerHTML = randomValue;
+  simbolo1.innerHTML = randomSuit;
+  simbolo2.innerHTML = randomSuit;
 };
